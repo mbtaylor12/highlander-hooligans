233880ba089cf3ee9eeecb017c9249ba1b01270d
mbtaylor12 <mbtaylor12@hotmail.com>
2017-10-27 14:40:32 -0400

    Added a function in organizationalStructures.php to convert json to 2D array. Updated hardware() to use a JSON string for data.

--- organizationStructures.php
+++ organizationStructures.php
@@ -1,5 +1,8 @@
 <?php
+	
 
+
+	error_reporting(E_ALL);
 	/*
 	*hardware() - Creates a collapisble card view for hardware.
 	*Calls function creatCollapsibleCardView using hardware data to get a hardware card view.
@@ -25,7 +28,102 @@
 			"Asset Tag" => "YHPC00001", "IP Address" => "10.1.23.2", 
 			"MAC Address" => "CC:33:55:C4:66:BB", "Location" => "Young Hall");
 
-		$cards = array($card1, $card2, $card3, $card4);
+		//$cards = array($card1, $card2, $card3, $card4);
+
+			$jsonString = '[
+ {
+   "assetNum": "RU10899",
+   "assetDesc": "Laptop",
+   "manuFac": "Lenovo",
+   "modelNum": "T420",
+   "serialNum": "77dj883",
+   "periphIncluded": "Charger",
+   "roomNum": "Loaner Cabinet",
+   "ticketStat": 99009
+ },
+ {
+   "assetNum": "RU10998",
+   "assetDesc": "Laptop",
+   "manuFac": "Lenovo",
+   "modelNum": "T430",
+   "serialNum": "88dha2j",
+   "periphIncluded": "Charger",
+   "roomNum": "Young 403 (Office)",
+   "ticketStat": 77890
+ },
+ {
+   "assetNum": "RU104556",
+   "assetDesc": "Tablet",
+   "manuFac": "Apple",
+   "modelNum": "iPad ",
+   "serialNum": "9088743",
+   "periphIncluded": "None",
+   "roomNum": "Loaner Cabinet",
+   "ticketStat": 97739
+ },
+ {
+   "assetNum": "RU109999",
+   "assetDesc": "Laptop",
+   "manuFac": "Apple",
+   "modelNum": "MacBook Pro",
+   "serialNum": "897kdha2",
+   "periphIncluded": "None",
+   "roomNum": "Loaner Cabinet",
+   "ticketStat": 987054
+ },
+ {
+   "assetNum": "RU109457",
+   "assetDesc": "T420 Laptop Dock",
+   "manuFac": "Lenovo",
+   "modelNum": "T6900",
+   "serialNum": "jd845773",
+   "periphIncluded": "Power Supply",
+   "roomNum": "Loaner Cabinet",
+   "ticketStat": 98999
+ },
+ {
+   "assetNum": "RU109766",
+   "assetDesc": "Laptop ",
+   "manuFac": "Leovo",
+   "modelNum": "T420 ",
+   "serialNum": "jjdk8872",
+   "periphIncluded": "Power Supply",
+   "roomNum": "Loaner Cabinet",
+   "ticketStat": 94776
+ },
+ {
+   "assetNum": "RU1085677",
+   "assetDesc": "Desktop",
+   "manuFac": "Lenovo ",
+   "modelNum": "6870",
+   "serialNum": "dasd937",
+   "periphIncluded": "None",
+   "roomNum": "Loaner Cabinet",
+   "ticketStat": 488729
+ },
+ {
+   "assetNum": "RU1098433",
+   "assetDesc": "Laptop",
+   "manuFac": "Apple",
+   "modelNum": "MacBook Pro (2012)",
+   "serialNum": "dad98sj",
+   "periphIncluded": "None",
+   "roomNum": "Loaner Cabinet",
+   "ticketStat": 345432
+ },
+ {
+   "assetNum": "RU1098374",
+   "assetDesc": "Desktop",
+   "manuFac": "Apple",
+   "modelNum": "iMac",
+   "serialNum": "3qef513",
+   "periphIncluded": "None",
+   "roomNum": "Davis 051",
+   "ticketStat": null
+ }
+]';
+
+		$cards = jsonTo2DArray($jsonString);
 
 		//Initializes the future 2D array.
 		$hardware = array();
@@ -34,23 +132,36 @@
 		foreach ($cards as $card)
 		{
 			//Assigns variables from associative array
-			$name = $card["Name"];
+			/*$name = $card["Name"];
 			$serialNumber = $card["Serial Number"];
 			$manufacturer = $card["Manufacturer"];
 			$connectionStatus = $card["Connection Status"];
 			$assetTag = $card["Asset Tag"];
 			$ipAddress = $card["IP Address"];
 			$macAddress = $card["MAC Address"];
-			$location = $card["Location"];
+			$location = $card["Location"];*/
+
+			$assetTag = $card['assetNum'];
+			$assetDesc = $card['assetDesc'];
+			$manuFac = $card['manuFac'];
+			$modelNum = $card['modelNum'];
+			$serialNum = $card['serialNum'];
+			$periphIncluded = $card['periphIncluded'];
+			$roomNum = $card['roomNum'];
+			$ticketStat = $card['ticketStat'];
+
+			$name = $assetTag;
 
 			//Creates the body of the card view in the layout I have chosen. 
 			//The class 'contentLeft'/'contentRight' determines which side the data will be on in the card view.
-			$content = "					<p class='contentLeft'>Manufacturer: $manufacturer </p>\n
-			 								<p class='contentRight'>Location: $location </p><br /><br />\n
-			 								<p class='contentLeft'>Connection Status: $connectionStatus </p><br /><br />\n
+			$content = "					<p class='contentLeft'>Manufacturer: $manuFac </p>\n
+			 								<p class='contentRight'>Location: $roomNum </p><br /><br />\n
+			 								<p class='contentLeft'>Model: $modelNum </p><br /><br />\n
+			 								<p class='contentLeft'>Serial Number: $serialNum </p><br /><br />\n
 			 								<p class='contentLeft'>Asset Tag: $assetTag </p><br /><br />\n
-			 								<p class='contentLeft'>IP Address: $ipAddress </p><br /><br />\n
-			 								<p class='contentLeft'>MAC Address: $macAddress </p>\n
+			 								<p class='contentLeft'>Asset Description: $assetDesc </p><br /><br />\n
+			 								<p class='contentLeft'>Periphereals: $periphIncluded </p>\n
+			 								<p class='contentLeft'>Ticket Status: $ticketStat </p><br /><br />\n
 			 								<p class='contentRight'><button>Apply</button></p>\n";	
 
 			//Assigns key-value pairs for the name and content.
@@ -102,4 +213,17 @@
 
 		return $html;
 	}
+
+	/*
+	*jsonTo2DArray - Converts JSON to 2D array
+	*@param $jsonString - String of JSON for converting
+	*@return 2D array of JSON objects
+	*/
+	function jsonTo2DArray($jsonString)
+	{
+		$jsonArray = json_decode($jsonString, true);
+
+		return $jsonArray;
+	}
+
 ?>

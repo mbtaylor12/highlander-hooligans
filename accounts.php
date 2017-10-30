38a85c14f804aded8db1feb3fc407ea7703cc326
dratliff19 <32547607+dratliff19@users.noreply.github.com>
2017-10-30 17:47:53 -0400

    Add files via upload

--- accounts.php
+++ accounts.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php require_once("master.php") ?>
+        
+        <?php require_once("master.php") ?>
+		<?php require_once("organizationStructures.php") ?>
 		<link rel="stylesheet" href="styles.css">
 		<script src="script.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<title>Accounts</title>
 	</head>
 	<body>
 		<?php echo makeHeader(4); ?>
+        
+       	<?php echo accounts(); ?>
+
+    
 	</body>
 </html>

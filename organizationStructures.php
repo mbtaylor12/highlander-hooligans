570e62320fe06c1f0ebf30813e68561ee4a3cd44
dratliff19 <32547607+dratliff19@users.noreply.github.com>
2017-10-30 17:46:49 -0400

    Updated for accounts() function

--- organizationStructures.php
+++ organizationStructures.php
@@ -1,9 +1,6 @@
 <?php
 	
-
-
 	error_reporting(E_ALL);
-
 	/*
 	*hardware() - Creates a collapisble card view for hardware.
 	*Calls function creatCollapsibleCardView using hardware data to get a hardware card view.
@@ -11,14 +8,10 @@
 	*/
 	function hardware()
 	{
-		$jsonString = exec('hardwareLoaner.py -s hardwareLoaners -all');
-
-
+			$jsonString = exec('hardwareLoaner.py -s hardwareLoaners -all');
 		$cards = jsonTo2DArray($jsonString);
-
 		//Initializes the future 2D array.
 		$hardware = array();
-
 		//Loops through each piece of hardware stored in 2D array
 		foreach ($cards as $card)
 		{
@@ -30,9 +23,7 @@
 			$periphIncluded = $card[5];
 			$roomNum = $card[6];
 			$ticketStat = $card[7];
-
 			$name = $assetTag;
-
 			//Creates the body of the card view in the layout I have chosen. 
 			//The class 'contentLeft'/'contentRight' determines which side the data will be on in the card view.
 			$content = "					<p class='contentLeft'>Manufacturer: <input type='text' class='notEditing' readonly='readonly' ondblclick='makeEditable(this)' onblur='stopEditing(this)' value='$manuFac'></p>\n
@@ -44,28 +35,56 @@
 			 								<p class='contentLeft'>Periphereals: <input type='text' class='notEditing' readonly='readonly' ondblclick='makeEditable(this)' onblur='stopEditing(this)' value='$periphIncluded'></p><br /><br />\n
 			 								<p class='contentLeft'>Ticket Status: <input type='text' class='notEditing' readonly='readonly' ondblclick='makeEditable(this)' onblur='stopEditing(this)' value='$ticketStat'></p><br />\n
 			 								<p class='contentRight'><button>Apply</button></p>\n";
-
 			//Assigns key-value pairs for the name and content.
 			$tempArray = array ("name" => $name, "content"=> $content);
 			//Pushes the $tempArray onto the $hardware array, creating a 2D array.
 			array_push($hardware, $tempArray);
 		}
-
 		return createCollapsibleView($hardware);
 	}
-
 	/*
 	*createCollapsibleView - Makes a collapsible card view.
 	*@param $cards - A 2D array where each sub-array's key-value pair is "name"=>$name and "content"=>$content.
 	*			     Name is the title which will appear on the card. Content is the body of the card.
 	*@return - HTML for a collapsible view.
 	*/
+
+
+
+    function accounts(){
+        
+        $jsonString = exec('hardwareLoaner.py -s Users -all');
+		$cards = jsonTo2DArray($jsonString);
+		//Initializes the future 2D array.
+		$users = array();
+		//Loops through each piece of hardware stored in 2D array
+		foreach ($cards as $card)
+		{
+			$user = $card[0];
+			$password = $card[1];
+			
+			$name = $user;
+			//Creates the body of the card view in the layout I have chosen. 
+			//The class 'contentLeft'/'contentRight' determines which side the data will be on in the card view.
+			$content = "					<p class='contentLeft'>Username: <input type='text' class='notEditing' readonly='readonly'                                                               ondblclick='makeEditable(this)' onblur='stopEditing(this)' value='$user'></p>\n     
+			 								<p class='contentRight'>Password: <input type='text' class='notEditing' readonly='readonly' ondblclick='makeEditable(this)' onblur='stopEditing(this)' value='$password'></p><br /><br />\n
+			 								<p class='contentRight'><button>Apply</button></p>\n";
+            
+			//Assigns key-value pairs for the name and content.
+			$tempArray = array ("name" => $user, "content"=> $content);
+			//Pushes the $tempArray onto the $hardware array, creating a 2D array.
+			array_push($users, $tempArray);
+		}
+		return createCollapsibleView($users);
+        
+        
+    }
+    
 	function createCollapsibleView(array $cards)
 	{
 		//Initial HTML for a card view
 		$html = "<div class='cardContainer'>\n"
 			  . "	<div class='panel-group' id='accordion'>\n";
-
 		//Loops through each element in $cards to pull out information about each card
 		foreach ($cards as $card) 
 		{
@@ -80,7 +99,7 @@
 				   . "					<a data-toggle='collapse' data-parent='#accordion' href='#$name'><div class='cardTitle'>$name</div></a>\n"
 				   . "				</h4>\n"
 				   . "			</div>\n"
-				   . "			<div id='$name' class='panel-collapse collapse collapse'>\n"
+				   . "			<div id='$user' class='panel-collapse collapse collapse'>\n"
 				   . "				<div class='panel-body'>\n"
 				   . "					$content"
 				   . "				</div>\n"
@@ -91,10 +110,8 @@
 		//Closing div tags from initial HTML
 		$html .= "	</div>\n"
 			   . "</div>\n";
-
 		return $html;
 	}
-
 	/*
 	*jsonTo2DArray - Converts JSON to 2D array
 	*@param $jsonString - String of JSON for converting
@@ -103,7 +120,6 @@
 	function jsonTo2DArray($jsonString)
 	{
 		$jsonArray = json_decode($jsonString, true);
-
 		return $jsonArray;
 	}
 ?>

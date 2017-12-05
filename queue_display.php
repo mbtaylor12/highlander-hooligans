300501ef829bdaad9bb82eb6e182899bb7e76132
mbtaylor12 <mbtaylor12@hotmail.com>
2017-12-05 13:06:49 -0500

    New queue_display page with CSS

--- queue_display.php
+++ queue_display.php
@@ -6,8 +6,14 @@ require_once("organizationStructures.php");
 <html>
 	<head>
 		<meta http-equiv="refresh" content="10">
+		<link rel="stylesheet" href="styles.css">
 	</head>
-	<body>
-		<?php echo queueWaitingTable(); ?>
+	<!--Royalty Free Background image from https://www.pexels.com/photo/usb-technology-green-microchip-57007/ -->
+	<body id="queuebody" style="background-repeat: no-repeat;" link="#ffffff" vlink="#ffffff" alink="#003a99">
+
+		<div class='queueDisplayDiv'>
+			<center><p style="font-size:30px;">Assistance Queue</p></center>
+			<?php echo queueWaitingTable(); ?>
+		</div>
 	</body> 
 </html>
